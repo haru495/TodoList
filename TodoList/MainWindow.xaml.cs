bbd37fd82d56f92427fed690e106e0a83ad27ced
hahaha01 <724021.wondaful@gmail.com>
2022-08-17 00:32:52 +0900

    TodoListを追加

---
+++ TodoList/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace TodoList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<TodoElement> todoList = new BindingList<TodoElement>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // ListBoxのTodoListで表示する内容を設定している
+        public void SetBindingList(object sender, RoutedEventArgs e)
+        {
+            TodoList.ItemsSource = todoList;
+        }
+
+        public void AddTodoList(object sender, RoutedEventArgs e)
+        {
+            todoList.Add(new TodoElement(TodoText.Text, false));
+        }
+
+        public void DeleteTodoList(object sender, RoutedEventArgs e)
+        {
+            BindingList<TodoElement> ListTmp = new BindingList<TodoElement>();
+
+            foreach(TodoElement element in todoList)
+            {
+                ListTmp.Add(element);
+            }
+
+            foreach(TodoElement element in ListTmp)
+            {
+                // CheckBoxでチェックが入るとBindingしているDoneFlgにtrueが入る
+                if(element.DoneFlg) todoList.Remove(element);
+            }
+        }
+    }
+}
---
+++ TodoList/TodoElement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class TodoElement
+{
+	public TodoElement(string Element, bool DoneFlg)
+	{
+		this.Element = Element;
+		this.DoneFlg = DoneFlg;
+	}
+
+	public string Element { get; set; }
+	public bool DoneFlg { get; set; }
+}
+
